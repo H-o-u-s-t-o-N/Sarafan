63097b7506dac251568dfe6d7b48038fe49fce42
HoustoN <realhyston@gmail.com>
2021-06-06 16:57:52 +0200

    Comments through WebSocket

--- src/main/java/HoustoN/Sarafan/domain/Message.java
+++ src/main/java/HoustoN/Sarafan/domain/Message.java
@@ -2,7 +2,9 @@ package HoustoN.Sarafan.domain;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -16,6 +18,10 @@ import java.util.List;
 @ToString(of = {"id", "text"})
 @EqualsAndHashCode(of = {"id"})
 @Data
+@JsonIdentityInfo(
+        property = "id",
+        generator = ObjectIdGenerators.PropertyGenerator.class
+)
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/HoustoN/Sarafan/service/CommentService.java
+++ src/main/java/HoustoN/Sarafan/service/CommentService.java
@@ -2,23 +2,33 @@ package HoustoN.Sarafan.service;
 
 import HoustoN.Sarafan.domain.Comment;
 import HoustoN.Sarafan.domain.User;
+import HoustoN.Sarafan.domain.Views;
+import HoustoN.Sarafan.dto.EventType;
+import HoustoN.Sarafan.dto.ObjectType;
 import HoustoN.Sarafan.repo.CommentRepo;
+import HoustoN.Sarafan.util.WsSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.function.BiConsumer;
 
 @Service
 public class CommentService {
     private final CommentRepo commentRepo;
+    private final BiConsumer<EventType, Comment> wsSender;
 
     @Autowired
-    public CommentService(CommentRepo commentRepo) {
+    public CommentService(CommentRepo commentRepo, WsSender wsSender) {
         this.commentRepo = commentRepo;
+        this.wsSender = wsSender.getSender(ObjectType.COMMENT, Views.FullComment.class);
     }
 
     public Comment create(Comment comment, User user) {
         comment.setAuthor(user);
-        commentRepo.save(comment);
+        Comment commentFromDb = commentRepo.save(comment);
 
-        return comment;
+        wsSender.accept(EventType.CREATE, commentFromDb);
+
+        return commentFromDb;
     }
 }
