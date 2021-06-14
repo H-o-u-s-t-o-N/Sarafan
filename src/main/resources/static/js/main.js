66eafe777998927f59c67a845578cb85a425c5b1
HoustoN <realhyston@gmail.com>
2021-06-14 22:22:38 +0200

    Add Sentry logging

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,6 +7,27 @@ import App from 'pages/App.vue'
 import store from 'store/store'
 import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
+import * as Sentry from '@sentry/browser'
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://8548c48e9a0c42149511e05469537a4e@o849342.ingest.sentry.io/5816265",
+
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+});
+
+Sentry.configureScope(scope =>
+    scope.setUser({
+        id: profile && profile.id,
+        username: profile && profile.name
+    })
+)
 
 if (profile) {
     connect()
